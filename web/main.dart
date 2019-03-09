27bbee1fef2c26e4e1904feddc1aa30cebdd7edf
s61494 <s61494@beuth-hochschule.de>
2019-03-10 00:25:06 +0100

    Add start and stop functionality

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,25 @@
+import 'dart:html';
 import 'Renderer.dart';
 import 'Engine.dart';
 
 Renderer renderer;
+HtmlElement startBtn;
+HtmlElement stopBtn;
+Engine engine;
 
 void main() {
+  startBtn = document.querySelector('#startBtn');
+  stopBtn = document.querySelector('#stopBtn');
+  startBtn.onClick.listen((e) => startGame(e));
+  stopBtn.onClick.listen((e) => stopGame(e));
   renderer = Renderer('#canvas');
-  Engine(renderer)..run();
+}
+
+void startGame(e) {
+  engine = Engine(renderer)..run();
+}
+
+void stopGame(e) {
+  engine.stop();
+  renderer.clear();
 }
